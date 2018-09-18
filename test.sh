ac959f0424498592e6be95a3c680bee6973337e0
altaf5 <altafshaikh8169@gmail.com>
2018-09-17 22:45:34 -0700

    added test.sh file

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "mkdir foss folder"
+mkdir foss
+
+echo "going to foss folder"
+cd foss
+
+echo "creating test_file.txt"
+touch test_file.txt
+
+echo ""
+echo "showing output of ls:"
+ls
+
+echo ""
+echo "updating linux repos"
+sudo apt-get update
